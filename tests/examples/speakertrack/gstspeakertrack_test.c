c7bfa3531b06f373e7fce24bfc7abcfeba14e195
Duzy Chan <code@duzy.info>
2012-11-29 09:52:30 +0800

    speakertrack: using video overlay

--- tests/examples/speakertrack/gstspeakertrack_test.c
+++ tests/examples/speakertrack/gstspeakertrack_test.c
@@ -1,14 +1,64 @@
+/*
+ * Speakertrack test
+ * Copyright (C) 2012 Duzy Chan <code@duzy.info>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
+ * which case the following provisions apply instead of the ones
+ * mentioned above:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#include <gdk/gdkx.h>
+#include <gtk/gtk.h>
 #include <gst/gst.h>
+#include <gst/video/videooverlay.h>
+
+static void
+window_closed (GtkWidget * widget, GdkEvent * event, gpointer user_data)
+{
+  GstElement *pipe = user_data;
+  gst_element_set_state (pipe, GST_STATE_NULL);
+  gtk_main_quit ();
+}
 
 static gboolean
 bus_observer (GstBus * bus, GstMessage * msg, gpointer data)
 {
-  GMainLoop *loop = (GMainLoop *) data;
-
   switch (GST_MESSAGE_TYPE (msg)) {
     case GST_MESSAGE_EOS:{
       g_print ("End-Of-Stream\n");
-      g_main_loop_quit (loop);
       break;
     }
 
@@ -22,9 +72,6 @@ bus_observer (GstBus * bus, GstMessage * msg, gpointer data)
 
       g_print ("Error: %s\n", err->message);
       g_error_free (err);
-
-      g_main_loop_quit (loop);
-
       break;
     }
 
@@ -194,10 +241,12 @@ add_file_source (GstElement * pipe, const char *filename, const char *profile)
 int
 main (int argc, char **argv)
 {
-  GstElement *bin;
+  GdkWindow *video_window_xwindow;
+  GtkWidget *window, *video_window;
+  GstElement *bin, *sink;
   GstBus *bus;
+  gulong embed_xid;
   int bus_watch_id;
-  GMainLoop *loop;
   const char *profile;
 
   if (argc == 2) {
@@ -210,6 +259,7 @@ main (int argc, char **argv)
   }
 
   gst_init (&argc, &argv);
+  gtk_init (&argc, &argv);
 
   bin = gst_pipeline_new ("speaker-tracking-bin");
   if (!bin) {
@@ -222,7 +272,11 @@ main (int argc, char **argv)
     return __LINE__;
   }
 
-  loop = g_main_loop_new (NULL, FALSE);
+  sink = gst_bin_get_by_name (GST_BIN (bin), "video-sink");
+  if (!sink) {
+    g_warning ("video-sink not created\n");
+    return __LINE__;
+  }
 
   bus = gst_element_get_bus (bin);
   if (!bus) {
@@ -230,17 +284,32 @@ main (int argc, char **argv)
     return __LINE__;
   }
 
-  bus_watch_id = gst_bus_add_watch (bus, bus_observer, loop);
+  bus_watch_id = gst_bus_add_watch (bus, bus_observer, NULL);
   g_object_unref (bus);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  g_signal_connect (G_OBJECT (window), "delete-event",
+      G_CALLBACK (window_closed), (gpointer) bin);
+  gtk_window_set_default_size (GTK_WINDOW (window), 640, 480);
+  gtk_window_set_title (GTK_WINDOW (window), "Speaker Track");
+
+  video_window = gtk_drawing_area_new ();
+  gtk_widget_set_double_buffered (video_window, FALSE);
+  gtk_container_add (GTK_CONTAINER (window), video_window);
+  gtk_container_set_border_width (GTK_CONTAINER (window), 5);
+
+  gtk_widget_show_all (window);
+  gtk_widget_realize (window);
+
+  video_window_xwindow = gtk_widget_get_window (video_window);
+  embed_xid = GDK_WINDOW_XID (video_window_xwindow);
+  gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (sink), embed_xid);
 
   gst_element_set_state (bin, GST_STATE_PLAYING);
 
-  g_main_loop_run (loop);
-
-  gst_element_set_state (bin, GST_STATE_NULL);
+  gtk_main ();
 
   g_object_unref (bin);
   g_source_remove (bus_watch_id);
-  g_main_loop_unref (loop);
   return 0;
 }
